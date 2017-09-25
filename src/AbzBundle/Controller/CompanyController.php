cb3c7d4374c95cbf077a2a8268bf4452278d8e65
Serge Boyko <boykodev@gmail.com>
2017-09-25 15:50:04 +0300

    add company controller

---
+++ src/AbzBundle/Controller/CompanyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AbzBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('company/index.html.twig');
+    }
+}
